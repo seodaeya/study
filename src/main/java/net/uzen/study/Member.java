0c5f95d8db1f026191c9a20fa44e283c56d1c839
key <dsseo.uzen@amorepacific.com>
2020-02-28 10:21:03 +0900

    Initial commit

---
+++ src/main/java/net/uzen/study/Member.java
@@ -0,0 +1,13 @@
+package net.uzen.study;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+}
